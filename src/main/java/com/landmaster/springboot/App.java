254eee1327703a89f1160e6b35e2e4333bca7e73
tong0711 <tong0711@163.com>
2017-01-12 10:17:41 +0800

    测试一下idea的使用

--- src/main/java/com/landmaster/springboot/App.java
+++ src/main/java/com/landmaster/springboot/App.java
@@ -4,10 +4,8 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 @EnableAutoConfiguration
 public class App  implements EmbeddedServletContainerCustomizer{
